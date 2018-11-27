2a3aa50c5cb0cec3742668143421584875eaf4d6
Michelle Cordeiro <mic321@gmail.com>
2018-11-27 17:15:25 -0300

    #02: Pasta static, pasta template e modificações nos arquivos .py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from .models import Inicio
-from .models import Quemsomos
-from .models import Servicos
-from .models import Depoimentos 
-from .models import Portfolio
-from .models import Parceiros
+from . import models
+# from . import forms
 
+admin.site.register(models.Inicio)
+admin.site.register(models.Quemsomos)
+admin.site.register(models.Servicos)
+admin.site.register(models.Depoimentos)
+admin.site.register(models.Portfolio)
+admin.site.register(models.Parceiros)
 
-admin.site.register(Inicio)
-admin.site.register(Quemsomos)
-admin.site.register(Servicos)
-admin.site.register(Depoimentos)
-admin.site.register(Portfolio)
-admin.site.register(Parceiros)
+# from .models import Inicio
+# from .models import Quemsomos
+# from .models import Servicos
+# from .models import Depoimentos 
+# from .models import Portfolio
+# from .models import Parceiros
+# admin.site.register(Inicio)
+# admin.site.register(Quemsomos)
+# admin.site.register(Servicos)
+# admin.site.register(Depoimentos)
+# admin.site.register(Portfolio)
+# admin.site.register(Parceiros)
--- mysite/views.py
+++ mysite/views.py
@@ -2,23 +2,23 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from .models import Inicio, Quemsomos, Servicos, Portfolio, Depoimentos, Parceiros
+#from django.http import HttpResponse
 
 def index(request):
-        inicio = inicio.objects.all()
-        quemsomos = Quemsomos.objects.all()
-        portfolio = portfolio.objects.all()
-        servicos = Servicos.objects.all() 
-        depoimentos = depoimentos.objects.all()
-        parceiros = parceiros.objects.all()
+    inicio = Inicio.objects.all()
+    quemsomos = Quemsomos.objects.all()
+    portfolio = Portfolio.objects.all()
+    servicos = Servicos.objects.all() 
+    depoimentos = Depoimentos.objects.all()
+    parceiros = Parceiros.objects.all()
 
-        context={
-                    'inicio':inicio,
-                    'quemsomos':quemsomos,
-                    'portfolio':portfolio,
-                    'servicos':servicos,
-                    'depoimentos':depoimentos,  
-                    'parceiros':parceiros,
-
-        }
+    context={
+        'inicio':inicio,
+        'quemsomos':quemsomos,
+        'portfolio':portfolio,
+        'servicos':servicos,
+        'depoimentos':depoimentos,  
+        'parceiros':parceiros,
+    }
        
-return render(request, 'index.html', context) 
+    return render(request, 'index.html', context) 
